32fd2d23dd88c83c6bf75de491da5c3180c0e1b7
Lucariatias <lucariatias@gmail.com>
2013-06-24 21:00:47 +0100

    Add race size modifiers

--- src/com/gildorymrp/api/plugin/core/Race.java
+++ src/com/gildorymrp/api/plugin/core/Race.java
@@ -7,15 +7,30 @@ package com.gildorymrp.api.plugin.core;
  */
 public enum Race {
 	
-	DROW,
-	DWARF,
-	ELF,
-	GNOME,
-	HALFELF,
-	HALFLING,
-	HALFORC,
-	HUMAN,
-	UNKNOWN,
-	OTHER;
+	DROW(0),
+	DWARF(1),
+	ELF(0),
+	GNOME(1),
+	HALFELF(0),
+	HALFLING(1),
+	HALFORC(0),
+	HUMAN(0),
+	UNKNOWN(0),
+	OTHER(0);
+	
+	private final int sizeModifier;
+	
+	private Race(final int sizeModifier) {
+		this.sizeModifier = sizeModifier;
+	}
+	
+	/**
+	 * Gets the size modifier of the race
+	 * 
+	 * @return the race's size modifier
+	 */
+	public int getSizeModifier() {
+		return sizeModifier;
+	}
 
 }
