8403eb444e11e77214b168755545ec689117b1f1
Lucariatias <lucariatias@gmail.com>
2013-07-09 17:35:45 +0100

    Update professions interfaces

--- src/com/gildorymrp/api/plugin/professions/GildorymProfessionsPlugin.java
+++ src/com/gildorymrp/api/plugin/professions/GildorymProfessionsPlugin.java
@@ -1,9 +1,9 @@
 package com.gildorymrp.api.plugin.professions;
 
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
 import com.gildorymrp.api.plugin.core.Character;
+import com.gildorymrp.api.plugin.equipment.DnDMaterial;
 
 /**
  * Represents a professions plugin
@@ -29,13 +29,29 @@ public interface GildorymProfessionsPlugin {
 	public Profession getProfession(Character character);
 	
 	/**
+	 * Sets the profession of a player's active character
+	 * 
+	 * @param player the player
+	 * @param profession the profession to set
+	 */
+	public void setProfession(Player player, Profession profession);
+	
+	/**
+	 * Sets the profession of a character
+	 * 
+	 * @param character the character
+	 * @param profession the profession to set
+	 */
+	public void setProfession(Character character, Profession profession);
+	
+	/**
 	 * Gets whether a player can craft the given material
 	 * 
 	 * @param player the player
 	 * @param material the material
 	 * @return true if the player can craft the material, otherwise false
 	 */
-	public boolean canCraft(Player player, Material material);
+	public boolean canCraft(Player player, DnDMaterial material);
 	
 	/**
 	 * Gets whether a player can smelt the given material
@@ -44,7 +60,7 @@ public interface GildorymProfessionsPlugin {
 	 * @param material the material
 	 * @return true if the player can smelt the material, otherwise false
 	 */
-	public boolean canSmelt(Player player, Material material);
+	public boolean canSmelt(Player player, DnDMaterial material);
 	
 	/**
 	 * Gets whether a player can use the given material
@@ -53,7 +69,7 @@ public interface GildorymProfessionsPlugin {
 	 * @param material the material
 	 * @return true if the player can use the material, otherwise false
 	 */
-	public boolean canUse(Player player, Material material);
+	public boolean canUse(Player player, DnDMaterial material);
 	
 	/**
 	 * Gets whether a player can interact with the given material
@@ -62,7 +78,43 @@ public interface GildorymProfessionsPlugin {
 	 * @param material the material
 	 * @return true if the player can use the material, otherwise false
 	 */
-	public boolean canInteract(Player player, Material material);
+	public boolean canInteract(Player player, DnDMaterial material);
+	
+	/**
+	 * Gets whether a character can craft the given material
+	 * 
+	 * @param character the character
+	 * @param material the material
+	 * @return true if the character can craft the material, otherwise false
+	 */
+	public boolean canCraft(Character character, DnDMaterial material);
+	
+	/**
+	 * Gets whether a character can smelt the given material
+	 * 
+	 * @param character the character
+	 * @param material the material
+	 * @return true if the character can smelt the material, otherwise false
+	 */
+	public boolean canSmelt(Character character, DnDMaterial material);
+	
+	/**
+	 * Gets whether a character can use the given material
+	 * 
+	 * @param character the character
+	 * @param material the material
+	 * @return true if the character can use the material, otherwise false
+	 */
+	public boolean canUse(Character character, DnDMaterial material);
+	
+	/**
+	 * Gets whether a character can interact with the given material
+	 * 
+	 * @param character the character
+	 * @param material the material
+	 * @return true if the character can use the material, otherwise false
+	 */
+	public boolean canInteract(Character character, DnDMaterial material);
 	
 	/**
 	 * Gets whether a profession can craft the given material
@@ -71,7 +123,7 @@ public interface GildorymProfessionsPlugin {
 	 * @param material the material
 	 * @return true if the profession can craft the material, otherwise false
 	 */
-	public boolean canCraft(Profession profession, Material material);
+	public boolean canCraft(Profession profession, DnDMaterial material);
 	
 	/**
 	 * Gets whether a profession can smelt the given material
@@ -80,7 +132,7 @@ public interface GildorymProfessionsPlugin {
 	 * @param material the material
 	 * @return true if the profession can smelt the material, otherwise false
 	 */
-	public boolean canSmelt(Profession profession, Material material);
+	public boolean canSmelt(Profession profession, DnDMaterial material);
 	
 	/**
 	 * Gets whether a profession can use the given material
@@ -89,7 +141,7 @@ public interface GildorymProfessionsPlugin {
 	 * @param material the material
 	 * @return true if the profession can use the material, otherwise false
 	 */
-	public boolean canUse(Profession profession, Material material);
+	public boolean canUse(Profession profession, DnDMaterial material);
 	
 	/**
 	 * Gets whether a profession can interact with the given material
@@ -98,6 +150,6 @@ public interface GildorymProfessionsPlugin {
 	 * @param material the material
 	 * @return true if the profession can use the material, otherwise false
 	 */
-	public boolean canInteract(Profession profession, Material material);
+	public boolean canInteract(Profession profession, DnDMaterial material);
 
 }
